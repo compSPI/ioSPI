28cf099211a1f94a52227f14335ce62bf6c5ec07
fredericpoitevin <frederic.poitevin@gmail.com>
2021-06-22 12:24:53 -0700

    attempt at setting up github workflows for linting and testing. Added unit tests after formatting the code.

--- iospi/iotools/atomic_models.py
+++ iospi/iotools/atomic_models.py
@@ -1,25 +1,116 @@
+"""Read and Write atomic models in various formats.
+"""
+
 import os
 
 import gemmi
 
 
-def file2model(filepath):
-    """file2model"""
-    _, file_ext = os.path.splitext(filepath)
-    if file_ext == ".cif":
-        cif_block = gemmi.cif.read(filepath)[0]
-        st = gemmi.make_structure_from_block(cif_block)
-    elif file_ext == ".pdb":
-        st = gemmi.read_structure(filepath)
-    st.remove_alternative_conformations()
-    st.remove_hydrogens()
-    st.remove_waters()
-    st.remove_ligands_and_waters()
-    st.remove_empty_chains()
-    if file_ext == ".cif":
+def read_gemmi_model(path, i_model=0, clean=True):
+    """Read PDB or mmCIF file.
+
+    Use Gemmi library to read PDB or mmCIF files and return a Gemmi model.
+    The hierarchy in Gemmi follows:
+    Structure - Model - Chain - Residue - Atom
+
+    Parameters
+    ----------
+    path: (string)
+        Path to PDB or mmCIF file.
+    i_model: (integer)
+        Optional, default: 0
+        Index of the returned model in the Gemmi Structure.
+    clean: (bool)
+        Optional, default: True
+        If True, use Gemmi remove_* methods to clean up structure.
+
+    Returns
+    -------
+    model: (Gemmi Class)
+        Gemmi model
+
+    Example
+    -------
+    TO DO
+
+    Reference
+    ---------
+    See https://gemmi.readthedocs.io/en/latest/mol.html for a definition of
+    gemmi objects.
+
+    """
+
+    try:
+        if not os.path.exists(path):
+            raise OSError
+        if path.lower().endswith('.pdb'):
+            st = gemmi.read_structure(path)
+        elif path.lower().endswith('.cif'):
+            cif_block = gemmi.cif.read(path)[0]
+            st = gemmi.make_structure_from_block(cif_block)
+        else:
+            raise Exception('File format not recognized.')
+    except OSError as ose:
+        print(ose)
+    except Exception as exc:
+        print(exc)
+
+    if clean:
+        st.remove_alternative_conformations()
+        st.remove_hydrogens()
+        st.remove_waters()
+        st.remove_ligands_and_waters()
+        st.remove_empty_chains()
+
+    if path.lower().endswith('.cif'):
         model = gemmi.make_assembly(
-            st.assemblies[0], st[0], gemmi.HowToNameCopiedChain.AddNumber
+            st.assemblies[i_model], st[i_model], gemmi.HowToNameCopiedChain.AddNumber
         )
-    elif file_ext == ".pdb":
-        model = st[0]
+    elif path.lower().endswith('.pdb'):
+        model = st[i_model]
+
     return model
+
+
+def write_gemmi_model(path, model=gemmi.Model()):
+    """Write Gemmi model to PDB or mmCIF file.
+
+    Use Gemmi library to write an atomic model to file.
+
+    Parameters
+    ----------
+    path: (string)
+        Path to PDB or mmCIF file.
+    model: (Gemmi Class)
+        Optional, default: gemmi.Model()
+        Gemmi model
+
+    Example
+    -------
+    TO DO
+
+    Reference
+    ---------
+    See https://gemmi.readthedocs.io/en/latest/mol.html for a definition of
+    gemmi objects.
+
+    """
+
+    try:
+        if path.lower().endswith('.pdb'):
+            pass
+        elif path.lower().endswith('.cif'):
+            pass
+        else:
+            raise Exception('File format not recognized.')
+    except Exception as exc:
+        print(exc)
+
+    structure = gemmi.Structure()
+    structure.add_model(model, pos=-1)
+    structure.renumber_models()
+
+    if path.lower().endswith('.cif'):
+        structure.make_mmcif_document().write_file(path)
+    elif path.lower().endswith('.pdb'):
+        structure.write_pdb(path)
---
+++ tests/tests_iotools/test_atomic_models.py
@@ -0,0 +1,59 @@
+"""Unit test for read/write of atomic models.
+"""
+
+import unittest
+
+from iotools.atomic_models import read_gemmi_model
+
+
+class TestAtomicModels(unittest.TestCase):
+    """Test for reading and writing atomic models."""
+
+    def test_write_gemmi_model_success(self):
+        """Write Gemmi model success test"""
+        path_pdb = '.test.pdb'
+        path_cif = '.test.cif'
+        expected = gemmi.Model()
+
+        write_gemmi_model(path_cif)
+        actual = read_gemmi_model(path_cif)
+        self.assertIs(actual, expected)
+
+        write_gemmi_model(path_pdb)
+        actual = read_gemmi_model(path_pdb)
+        self.assertIs(actual, expected)
+
+    def test_read_gemmi_model_success(self):
+        """Read Gemmi model success test"""
+        path_pdb = '.test.pdb'
+        path_cif = '.test.cif'
+        expected = gemmi.Model()
+
+        write_gemmi_model(path_cif)
+        actual = read_gemmi_model(path_cif)
+        self.assertIs(actual, expected)
+
+        write_gemmi_model(path_pdb)
+        actual = read_gemmi_model(path_pdb)
+        self.assertIs(actual, expected)
+
+
+    def test_read_gemmi_model_oserror(self):
+        """Read Gemmi OSError test"""
+        path='non-existing-file.pdb'
+        with self.assertRaises(OSError) as exception_context:
+            _ = read_gemmi_model(path)
+        self.assertEqual(
+            str(exception_context.exception), OSError)
+        )
+
+    def test_read_gemmi_model_filename_extension_error(self):
+        """Read Gemmi Filename Extension Error"""
+        path = '.test.txt'
+        with open(path, 'w'):
+            pass
+        with self.assertRaises(Exception) as exception_context:
+            _ = read_gemmi_model(path)
+        self.assertEqual(
+            str(exception_context.exception), 'File format not recognized.')
+        )
