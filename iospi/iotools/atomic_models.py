bed45e61f4ed31ecfa1533e4bdada2afa2333b7e
fredericpoitevin <frederic.poitevin@gmail.com>
2021-06-25 15:51:42 -0700

    Split `read_gemmi_model` function in 3: one for handling exceptions, one dedicated to PDB files, the other mmCIF files.

--- iospi/iotools/atomic_models.py
+++ iospi/iotools/atomic_models.py
@@ -39,42 +39,101 @@ def read_gemmi_model(path, i_model=0, clean=True):
 
     """
     try:
-        if path.lower().endswith(".pdb") or path.lower().endswith(".cif"):
-            if not os.path.isfile(path):
-                raise OSError
+        if os.path.isfile(path):
+            if path.lower().endswith(".pdb"):
+                return read_gemmi_model_from_pdb(path, i_model, clean)
+            elif path.lower().endswith(".cif"):
+                return read_gemmi_model_from_cif(path, i_model, clean)
+            else:
+                raise ValueError("File format not recognized.")
         else:
-            raise ValueError("File format not recognized.")
+            raise OSError
     except OSError as ose:
         print(type(ose), "::", ose)
     except ValueError as ve:
         print(type(ve), "::", ve)
 
-    st = None
 
-    if path.lower().endswith(".pdb"):
-        st = gemmi.read_structure(path)
-    elif path.lower().endswith(".cif"):
-        cif_block = gemmi.cif.read(path)[0]
-        st = gemmi.make_structure_from_block(cif_block)
+def read_gemmi_model_from_pdb(path, i_model=0, clean=True):
+    """Read Gemmi Model from PDB file.
 
+    Parameters
+    ----------
+    path: string
+        Path to PDB file.
+    i_model: integer
+        Optional, default: 0
+        Index of the returned model in the Gemmi Structure.
+    clean: bool
+        Optional, default: True
+        If True, use Gemmi remove_* methods to clean up structure.
+
+    Returns
+    -------
+    model: Gemmi Class
+        Gemmi model
+    """
+
+    structure = gemmi.read_structure(path)
     if clean:
-        st.remove_alternative_conformations()
-        st.remove_hydrogens()
-        st.remove_waters()
-        st.remove_ligands_and_waters()
-        st.remove_empty_chains()
-
+        structure = clean_gemmi_structure(structure)
     model = st[i_model]
-    if path.lower().endswith(".cif"):
-        assembly = st.assemblies[i_model]
-        model = gemmi.make_assembly(
-            assembly, model, gemmi.HowToNameCopiedChain.AddNumber
-        )
-    # elif path.lower().endswith(".pdb"):
-    #    model = st[i_model]
-
     return model
 
+def read_gemmi_model_from_cif(path, i_model=0, clean=True):
+    """Read Gemmi Model from CIF file.
+
+    Parameters
+    ----------
+    path: string
+        Path to mmCIF file.
+    i_model: integer
+        Optional, default: 0
+        Index of the returned model in the Gemmi Structure.
+    clean: bool
+        Optional, default: True
+        If True, use Gemmi remove_* methods to clean up structure.
+
+    Returns
+    -------
+    model: Gemmi Class
+        Gemmi model
+    """
+
+    cif_block = gemmi.cif.read(path)[0]
+    structure = gemmi.make_structure_from_block(cif_block)
+    if clean:
+        structure = clean_gemmi_structure(structure)
+    model = st[i_model]
+    assembly = st.assemblies[i_model]
+    model = gemmi.make_assembly(
+        assembly, model, gemmi.HowToNameCopiedChain.AddNumber
+    )
+    return model
+
+def clean_gemmi_structure(structure=None):
+    """Clean Gemmi Structure.
+
+    Parameters
+    ----------
+    structure: Gemmi Class
+        Gemmi Structure object
+
+    Returns
+    -------
+    structure: Gemmi Class
+        Same object, cleaned up of unnecessary atoms.
+
+    """
+
+    if structure is not None:
+        structure.remove_alternative_conformations()
+        structure.remove_hydrogens()
+        structure.remove_waters()
+        structure.remove_ligands_and_waters()
+        structure.remove_empty_chains()
+
+    return structure
 
 def write_gemmi_model(path, model=gemmi.Model("model")):
     """Write Gemmi model to PDB or mmCIF file.
