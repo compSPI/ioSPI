f1cdf22c530bb2c32011c6ec9bcccbd9b10add64
fredericpoitevin <frederic.poitevin@gmail.com>
2021-06-25 16:59:41 -0700

    addresses flake8 concern

--- tests/tests_iotools/test_atomic_models.py
+++ tests/tests_iotools/test_atomic_models.py
@@ -12,24 +12,28 @@ class TestAtomicModels(unittest.TestCase):
 
     def test_read_or_write_gemmi_model_success(self):
         """Read Gemmi model success test"""
+        expected = gemmi.Model("model")
         for path in ".test.pdb" ".test.cif":
             write_gemmi_model(path)
-            self.assertIs(read_gemmi_model(path), gemmi.Model("model"))
+            actual = read_gemmi_model(path)
+            self.assertIs(actual, expected)
 
     def test_read_gemmi_model_oserror(self):
         """Read Gemmi OSError test"""
         path = "non-existing-file.pdb"
+        expected = "File could not be found."
         with self.assertRaises(OSError) as exception_context:
             _ = read_gemmi_model(path)
-        self.assertEqual(str(exception_context.exception), "File could not be found.")
+        actual = str(exception_context.exception)
+        self.assertEqual(actual, expected)
 
     def test_read_gemmi_model_filename_extension_error(self):
         """Read Gemmi Filename Extension Error"""
         path = ".test.txt"
+        expected = "File format not recognized."
         with open(path, "w"):
             pass
         with self.assertRaises(Exception) as exception_context:
             _ = read_gemmi_model(path)
-        self.assertEqual(
-            str(exception_context.exception), "File format not recognized."
-        )
+        actual = str(exception_context.exception)
+        self.assertEqual(actual, expected)
