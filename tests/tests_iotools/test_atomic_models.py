c2b4fc46ae58829aaa13bf30194bce98d57e46f9
fredericpoitevin <frederic.poitevin@gmail.com>
2021-06-25 17:18:24 -0700

    fix some gemmi logic for Model naming

--- tests/tests_iotools/test_atomic_models.py
+++ tests/tests_iotools/test_atomic_models.py
@@ -14,7 +14,7 @@ class TestAtomicModels(unittest.TestCase):
         """Read Gemmi model success test"""
         expected = gemmi.Model("model")
         for path in ["test.pdb", "test.cif"]:
-            write_gemmi_model(path)
+            write_gemmi_model(path, model=expected)
             actual = read_gemmi_model(path)
             self.assertIs(actual, expected)
 
