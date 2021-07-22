d1c6983dfa3fe1d80559b32deae4461f92d7271f
NicolasLegendre1 <nicolaslegendre963@yahoo.com>
2021-07-22 23:54:13 +0200

    Open datasets

--- tests/test_iotools/test_cryo_dataset.py
+++ tests/test_iotools/test_cryo_dataset.py
@@ -1,11 +1,12 @@
 """Test cryo_datasets."""
 import torch
 import numpy as np
-from iospi.iotools import cryo_dataset
+from ioSPI.iotools import cryo_dataset
 
 
 class TestDataset:
-    def test_normalize_torch(self):
+    @staticmethod
+    def test_normalize_torch():
         dataset = torch.Tensor(
             [[3.0, 7.0, 2.0, 7.0], [3.0, 0.0, 8.0, 3.0], [6.0, 7.0, 4.0, 2.0]]
         )
@@ -22,7 +23,8 @@ class TestDataset:
         assert torch.equal(result, expected)
         assert type(result) is torch.Tensor
 
-    def test_split_dataset(self):
+    @staticmethod
+    def test_split_dataset():
         frac_val = 0.2
         batch_size = 20
         dataset = torch.Tensor(np.ones((2000, 1, 64, 64)))
@@ -38,13 +40,15 @@ class TestDataset:
         assert type(tr_s) is torch.utils.data.dataset.Subset
         assert type(ts_s) is torch.utils.data.dataset.Subset
 
-    def test_hinted_tuple_hook(self):
+    @staticmethod
+    def test_hinted_tuple_hook():
         dic1 = {"items": [4, 6], "__tuple__": True}
         list1 = [4, 6]
         assert cryo_dataset.hinted_tuple_hook(dic1) == (4, 6)
         assert cryo_dataset.hinted_tuple_hook(list1) == [4, 6]
 
-    def test_open_dataset(self):
+    @staticmethod
+    def test_open_dataset():
         path = "./tests/test_iotools/data_test_cryo_dataset.npy"
         dataset1 = cryo_dataset.open_dataset(path, size=64, is_3d=False)
         dataset2 = cryo_dataset.open_dataset(path, size=32, is_3d=False)
@@ -52,7 +56,8 @@ class TestDataset:
         assert dataset1.shape == torch.Size([1, 1, 64, 64])
         assert dataset2.shape == torch.Size([1, 1, 32, 32])
 
-    def test_load_parameters(self):
+    @staticmethod
+    def test_load_parameters():
         path = "./tests/test_iotools/vae_parameters.json"
         parameters = cryo_dataset.load_parameters(path)
         assert len(parameters) == 5
