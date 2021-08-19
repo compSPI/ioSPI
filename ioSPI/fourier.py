cb683b1b2a05f2251fe77cb6de8ff3073d5201ae
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-08-18 20:38:16 -0700

    docstrings

--- ioSPI/fourier.py
+++ ioSPI/fourier.py
@@ -4,7 +4,46 @@ from numba import jit
 
 
 @jit
+def neg_pos_2d(arr2d):
+    """Apply checkerboard pattern.
+
+    Each pixel switches from positive to negative in checker board pattern.
+
+    Parameters
+    ----------
+    arr2d : numpy.ndarray, shape (N,N)
+        Input array.
+
+    Returns
+    -------
+    arr2d : numpy.ndarray, shape (N,N)
+        Output array.
+    """
+    assert arr2d.ndim == 3  # extra axis
+    for n_particle in range(arr2d.shape[0]):
+        for r in range(arr2d.shape[1]):
+            for c in range(arr2d.shape[2]):
+                if (r + c) % 2:
+                    arr2d[n_particle, r, c] *= -1
+    return arr2d
+
+
+@jit
 def make_neg_pos_3d(arr3d):
+    """Apply checkerboard pattern.
+
+    Each pixel switches from positive to negative in checker board pattern.
+
+    Parameters
+    ----------
+    arr3d : numpy.ndarray, shape (N,N,N)
+        Input array.
+
+    Returns
+    -------
+    neg_pos_3d : numpy.ndarray, shape (N,N,N)
+        Output array.
+    """
     R1, R2, R3 = arr3d.shape
     neg_pos_3d = np.ones(arr3d.shape)
     for r1 in range(R1):
@@ -18,6 +57,28 @@ def make_neg_pos_3d(arr3d):
 def fft3d(
     arr3d, mode, neg_pos_3d=None, numpy_fft=pyfftw.interfaces.numpy_fft, only_real=False
 ):
+    """3D FFT
+
+    Fast fourier transform
+
+    Parameters
+    ----------
+    arr3d : numpy.ndarray, shape (N,N,N)
+        Input array.
+    mode : str
+        Forward or reverse
+    neg_pos_3d : numpy.ndarray, shape (N,N,N)
+        Optinal precomputed array. If None (default) passed, computes on the fly.
+    numpy_fft : func
+        Function backend for performing the fft.
+    only_real : bool
+        If True, return only real part of transform.
+
+    Returns
+    -------
+    arr3d_f : numpy.ndarray, shape (N,N,N)
+        Output array.
+    """
     if neg_pos_3d is None:
         neg_pos_3d = make_neg_pos_3d(arr3d)
     if arr3d.shape[0] % 4 != 0:
@@ -38,33 +99,88 @@ def fft3d(
 
 
 def do_fft(arr, d=3, only_real=False, **kwargs):
-    assert d in [2, 3], "only 2d/3d implemented"
+    """FFT wrapper.
+
+    Wraps fft2d
+
+    Parameters
+    ----------
+    arr : numpy.ndarray, shape (N,N) or (N,N,N)
+        Input array.
+    d : int, 2 or 3
+        Dimension.
+    only_real : bool
+        If True, return only real part of transform.
+        
+    Returns
+    -------
+    arr_f : numpy.ndarray, shape (N,N) or (N,N,N)
+        Output array.
+    """
     if d == 2:
-        return fft2d(arr, mode="f", **kwargs)
+        arr_f = fft2d(arr, mode="f", **kwargs)
     elif d == 3:
-        return fft3d(arr, mode="f", **kwargs)
+        arr_f = fft3d(arr, mode="f", **kwargs)
+    return arr_f
 
 
-def do_ifft(arr, d=3, only_real=True, **kwargs):
-    assert d in [2, 3], "only 2d/3d implemented"
+def do_ifft(arr_f, d=3, only_real=True, **kwargs):
+    """iFFT wrapper.
+
+    Wraps fft2d
+
+    Parameters
+    ----------
+    arr_f : numpy.ndarray, shape (N,N) or (N,N,N)
+        Input array.
+    d : int, 2 or 3
+        Dimension.
+    only_real : bool
+        If True, return only real part of transform.
+        
+    Returns
+    -------
+    arr : numpy.ndarray, shape (N,N) or (N,N,N)
+        Output array.
+    """
     if d == 2:
-        return fft2d(arr, mode="i", only_real=only_real, **kwargs)
+        arr = fft2d(arr_f, mode="i", only_real=only_real, **kwargs)
     elif d == 3:
-        return fft3d(arr, mode="i", only_real=only_real, **kwargs)
+        arr = fft3d(arr_f, mode="i", only_real=only_real, **kwargs)
+    return arr
 
 
 def fft2d(
     arr2d, mode, numpy_fft=pyfftw.interfaces.numpy_fft, only_real=False, batch=False
 ):
-    """
-    we apply an alterating +1/-1 multiplicative before we go to/from Fourier space.
-    Later we apply this again to the transform.
-    TODO: look into pyfftw.interfaces.numpy_fft.irfftn
-    """
+    """2D FFT
 
+    Fast fourier transform
+    TODO: look into pyfftw.interfaces.numpy_fft.irfftn .
+
+    Parameters
+    ----------
+    arr2d : numpy.ndarray, shape (N,N) or (num_exposures,N,N)
+        Input array.
+    mode : str
+        Forward or reverse
+    numpy_fft : func
+        Function backend for performing the fft.
+    only_real : bool
+        If True, return only real part of transform.
+    batch : bool
+        Use for batch of exposures.
+
+    Returns
+    -------
+    arr2d_f : numpy.ndarray, shape (N,N) or (num_exposures,N,N)
+        Output array.
+    """
     assert (arr2d.ndim == 2 and not batch) or (batch and arr2d.ndim == 3)
     n1, n2 = arr2d.shape[-2:]
     assert n1 == n2
+    # we apply an alterating +1/-1 multiplicative before we go to/from Fourier space.
+    # Later we apply this again to the transform.
     arr2d = neg_pos_2d(arr2d.reshape(-1, n1, n1).copy())
 
     if mode == "f":
@@ -83,16 +199,3 @@ def fft2d(
 
     return arr2d_f
 
-
-@jit
-def neg_pos_2d(arr2d):
-    """
-    each pixel switches from positive to negative in checker board pattern
-    """
-    assert arr2d.ndim == 3  # extra axis
-    for n_particle in range(arr2d.shape[0]):
-        for r in range(arr2d.shape[1]):
-            for c in range(arr2d.shape[2]):
-                if (r + c) % 2:
-                    arr2d[n_particle, r, c] *= -1
-    return arr2d
