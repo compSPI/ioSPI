82776b0f7792ee03312c1595f1895e510de71f4b
Geoffrey Woollard <geoffwoollard@gmail.com>
2021-08-24 20:00:24 -0400

    N to n_pixels

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -4,27 +4,27 @@ from ioSPI import transfer
 
 def test_ctf_freqs():
     """Test for ctf_freq."""
-    N = (np.random.randint(low=8, high=128) // 2) * 2
-    freq_1d = transfer.ctf_freqs(N, d=1)
-    assert freq_1d.shape == (N // 2,)
+    n_pixels = (np.random.randint(low=8, high=128) // 2) * 2
+    freq_1d = transfer.ctf_freqs(n_pixels, d=1)
+    assert freq_1d.shape == (n_pixels // 2,)
     assert np.isclose(freq_1d.min(), 0)
     assert np.isclose(freq_1d[0], 0)
-    assert freq_1d[-1] < N // 2
+    assert freq_1d[-1] < n_pixels // 2
     psize = np.random.uniform(low=1, high=10)
-    freq_A_2d, angles_rad = transfer.ctf_freqs(N, d=2, psize=psize)
-    assert np.isclose(freq_A_2d[N // 2, 0], 0.5 * psize)
-    assert np.isclose(freq_A_2d[N // 2, N // 2], 0)  # dc
-    assert freq_A_2d.shape == angles_rad.shape == (N, N)
+    freq_mag_2d, angles_rad = transfer.ctf_freqs(n_pixels, d=2, psize=psize)
+    assert np.isclose(freq_mag_2d[n_pixels // 2, 0], 0.5 * psize)
+    assert np.isclose(freq_mag_2d[n_pixels // 2, n_pixels // 2], 0)  # dc
+    assert freq_mag_2d.shape == angles_rad.shape == (n_pixels, n_pixels)
     assert (-np.pi <= angles_rad.min()) and (angles_rad.max() <= np.pi)
 
 
 def test_eval_ctf():
     """Test for eval_ctf."""
-    N = (np.random.randint(low=8, high=128) // 2) * 2
-    freq_A_2d, angles_rad = transfer.ctf_freqs(N, d=2)
+    n_pixels = (np.random.randint(low=8, high=128) // 2) * 2
+    freq_mag_2d, angles_rad = transfer.ctf_freqs(n_pixels, d=2)
     ac = np.random.uniform(low=0.07, high=0.1)
     ctf = transfer.eval_ctf(
-        s=freq_A_2d,
+        s=freq_mag_2d,
         a=angles_rad,
         def1=1e4,
         def2=1.1e4,
@@ -36,14 +36,14 @@ def test_eval_ctf():
         bf=0,
         lp=0,
     )
-    assert ctf.shape == (N, N)
-    assert np.isclose(ctf[N // 2, N // 2], ac)
+    assert ctf.shape == (n_pixels, n_pixels)
+    assert np.isclose(ctf[n_pixels // 2, n_pixels // 2], ac)
     assert -1 <= ctf.min() and ctf.max() <= 1
 
 
 def test_random_ctfs():
     """Test for random_ctfs."""
-    N = (np.random.randint(low=8, high=128) // 2) * 2
+    n_pixels = (np.random.randint(low=8, high=128) // 2) * 2
     n_particles = np.random.randint(low=3, high=7)
     df_min = np.random.uniform(low=5000, high=30000)
     df_max = df_min + 100
@@ -52,7 +52,7 @@ def test_random_ctfs():
     df_ang_min = np.random.uniform(low=0, high=360)
     df_ang_max = np.random.uniform(low=df_ang_min, high=360)
     ctfs, df1s, df2s, df_ang_deg = transfer.random_ctfs(
-        N,
+        n_pixels,
         psize=1,
         n_particles=n_particles,
         df_min=df_min,
@@ -68,7 +68,7 @@ def test_random_ctfs():
         bf=0,
         do_log=False,
     )
-    assert ctfs.shape == (n_particles, N, N)
+    assert ctfs.shape == (n_particles, n_pixels, n_pixels)
     for dfs in [df1s, df2s]:
         assert df_min - df_diff_max <= dfs.min()
         assert dfs.max() <= df_max + df_diff_max
