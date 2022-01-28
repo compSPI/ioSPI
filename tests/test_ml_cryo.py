5fb71215bf53d864033142497bbee3cc3ff3e7f9
Nina Miolane <nmiolane@gmail.com>
2022-01-28 13:28:37 -0800

    Rename cryo for ml

--- tests/test_ml_cryo.py
+++ tests/test_ml_cryo.py
@@ -1,9 +1,9 @@
-"""Test cryo_datasets."""
+"""Test ml_cryos."""
 
 import numpy as np
 import torch
 
-from ioSPI import cryodataset as cryo_dataset
+from ioSPI import ml_cryo
 
 
 class TestDataset:
@@ -16,7 +16,7 @@ class TestDataset:
             [[3.0, 7.0, 2.0, 7.0], [3.0, 0.0, 8.0, 3.0], [6.0, 7.0, 4.0, 2.0]]
         )
         dataset = dataset.reshape((1, 4, 3))
-        result = cryo_dataset.normalize_torch(dataset)
+        result = ml_cryo.normalize_torch(dataset)
         expected = torch.Tensor(
             [
                 [0.375, 0.875, 0.25, 0.875],
@@ -34,9 +34,7 @@ class TestDataset:
         frac_val = 0.2
         batch_size = 20
         dataset = torch.Tensor(np.ones((2000, 1, 64, 64)))
-        tr_s, ts_s, tr_l, ts_l = cryo_dataset.split_dataset(
-            dataset, batch_size, frac_val
-        )
+        tr_s, ts_s, tr_l, ts_l = ml_cryo.split_dataset(dataset, batch_size, frac_val)
         assert len(tr_s) == 1600
         assert len(ts_s) == 400
         assert len(tr_l) == 80
@@ -51,15 +49,15 @@ class TestDataset:
         """Test test_hinted_tuple_hook."""
         dic1 = {"items": [4, 6], "__tuple__": True}
         list1 = [4, 6]
-        assert cryo_dataset.hinted_tuple_hook(dic1) == (4, 6)
-        assert cryo_dataset.hinted_tuple_hook(list1) == [4, 6]
+        assert ml_cryo.hinted_tuple_hook(dic1) == (4, 6)
+        assert ml_cryo.hinted_tuple_hook(list1) == [4, 6]
 
     @staticmethod
     def test_open_dataset():
         """Test test_open_dataset."""
-        path = "./tests/data_test_cryo_dataset.npy"
-        dataset1 = cryo_dataset.open_dataset(path, size=64, is_3d=False)
-        dataset2 = cryo_dataset.open_dataset(path, size=32, is_3d=False)
+        path = "./tests/data_test_ml_cryo.npy"
+        dataset1 = ml_cryo.open_dataset(path, size=64, is_3d=False)
+        dataset2 = ml_cryo.open_dataset(path, size=32, is_3d=False)
         assert type(dataset1) is torch.Tensor
         assert dataset1.shape == torch.Size([1, 1, 64, 64])
         assert dataset2.shape == torch.Size([1, 1, 32, 32])
@@ -68,7 +66,7 @@ class TestDataset:
     def test_load_parameters():
         """Test test_load_parameters."""
         path = "./tests/vae_parameters.json"
-        parameters = cryo_dataset.load_parameters(path)
+        parameters = ml_cryo.load_parameters(path)
         assert len(parameters) == 5
         assert "skip_z" in parameters[2].keys()
         assert "enc_c" in parameters[2].keys()
