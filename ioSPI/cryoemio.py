05566c361eb6137af395185242c4f13d94d5cf78
arjunsingh3600 <swaniarjun@gmail.com>
2022-01-28 04:34:10 -0800

    changed defocus format

--- ioSPI/cryoemio.py
+++ ioSPI/cryoemio.py
@@ -383,5 +383,36 @@ def write_defocus_file(defocus_sample,defocus_file):
 
     """
     with open(defocus_file, "w") as inp:
+        inp.write("# File created by TEM-simulator, version 1.3.\n")
+        inp.write(f"{len(defocus_sample)} 1\n")
         for sample in defocus_sample:
             inp.write(f"{sample}\n")
+
+def write_crd_file(
+    n_particles,
+    xrange=np.arange(-100, 110, 10),
+    yrange=np.arange(-100, 110, 10),
+    crd_file="crd.txt",
+):
+    """Write particle data to .txt file containing particle stack data.
+
+    Particle center coordinates as well as its Euler angles is written to file.
+
+    Parameters
+    ----------
+    n_particles : int
+        Number of particles.
+    xrange : ndarray
+        Range of particle center x-coordinates to write.
+    yrange : ndarray
+        Range of particle center y-coordinates to write.
+    crd_file : str
+        Relative path to output .txt file.
+    """
+    log = logging.getLogger()
+
+    if os.path.exists(crd_file):
+        log.info(crd_file + " already exists.")
+    else:
+        rotlist = get_rotlist(n_particles)
+        n = 0
