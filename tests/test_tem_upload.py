2f01fa13b3348dd17a14431dc28d69cff1d59500
jed <jed324@gmail.com>
2022-01-27 18:29:13 -0800

    added gh secret token

--- tests/test_tem_upload.py
+++ tests/test_tem_upload.py
@@ -14,7 +14,7 @@ from ioSPI import tem_upload
 @pytest.fixture(autouse=True, scope="session")
 def setup_teardown():
     """Test node creation and clean-up for tests."""
-    token = ""
+    token = os.environ['TEST_TOKEN']
     request_headers = {"Authorization": f"Bearer {token}"}
     base_api_url = "https://api.osf.io/v2/nodes/"
 
