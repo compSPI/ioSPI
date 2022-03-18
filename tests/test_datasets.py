3e7503e95057b3495c069a5db18208b4d08cede4
arjunsingh3600 <swaniarjun@gmail.com>
2022-03-17 21:18:14 -0700

    removed debugging statement

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -14,8 +14,6 @@ from ..ioSPI import datasets
 def setup_teardown():
     """Test node creation and clean-up for tests."""
     token = os.environ["TEST_TOKEN"]
-    print("THis token will be removed and github secret will be updated")
-    print(token)
     request_headers = {"Authorization": f"Bearer {token}"}
     base_api_url = "https://api.osf.io/v2/nodes/"
 
