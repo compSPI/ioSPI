9efe0f5e713af0c164a0ce3b702eb089bc633ad6
fredericpoitevin <frederic.poitevin@gmail.com>
2022-03-27 19:03:03 -0700

    Imported from cryonettorch with slight edits from Ariana's code.

--- ioSPI/atomic_models.py
+++ ioSPI/atomic_models.py
@@ -1,5 +1,6 @@
 """Read and write atomic models in various formats."""
 
+import itertools
 import os
 
 import gemmi
@@ -146,6 +147,84 @@ def clean_gemmi_structure(structure=None):
         structure.remove_empty_chains()
 
     return structure
+
+
+def extract_gemmi_atoms(model, chains=None, split_chains=False):
+    """
+    Extract Gemmi atoms from the input Gemmi model, separated by chain.
+
+    Parameters
+    ----------
+    model : Gemmi Class
+        Gemmi model
+    chains : list of strings
+        chains to select, optional.
+        If not provided, retrieve atoms from all chains.
+    split_chains : bool
+        Optional, default: False
+        if True, keep the atoms from different chains in separate lists
+
+    Returns
+    -------
+    atoms : list (or list of list(s)) of Gemmi atoms
+        Gemmi atom objects, either concatenated or separated by chain
+    """
+    if chains is None:
+        chains = [ch.name for ch in model]
+
+    atoms = []
+    for ch in model:
+        if ch.name in chains:
+            atoms.append([at for res in ch for at in res])
+
+    if not split_chains:
+        atoms = list(itertools.chain.from_iterable(atoms))
+
+    return atoms
+
+
+def extract_atomic_parameter(atoms, parameter_type, split_chains=False):
+    """
+    Interpret Gemmi atoms and extract a single parameter type.
+
+    Parameters
+    ----------
+    atoms : list (of list(s)) of Gemmi atoms
+        Gemmi atom objects associated with each chain
+    parameter_type : string
+        'cartesian_coordinates', 'form_factor_a', or 'form_factor_b'
+    split_chains : bool
+        Optional, default: False
+        if True, keep the atoms from different chains in separate lists
+
+    Returns
+    -------
+    atomic_parameter : list of floats, or list of lists of floats
+        atomic parameter associated with each atom, optionally split by chain
+    """
+    # if list of Gemmi atoms, convert into a list of list
+    if type(atoms[0]) != list:
+        atoms = [atoms]
+
+    if parameter_type == "cartesian_coordinates":
+        atomic_parameter = [at.pos.tolist() for ch in atoms for at in ch]
+    elif parameter_type == "electron_form_factor_a":
+        atomic_parameter = [at.element.c4322.a for ch in atoms for at in ch]
+    elif parameter_type == "electron_form_factor_b":
+        atomic_parameter = [at.element.c4322.b for ch in atoms for at in ch]
+    else:
+        raise ValueError("Atomic parameter type not recognized.")
+        return
+
+    # optionally preserve the list of lists (separated by chain) structure
+    if split_chains:
+        reshape = [0] + [len(ch) for ch in atoms]
+        atomic_parameter = [
+            atomic_parameter[reshape[i] : reshape[i] + reshape[i + 1]]
+            for i in range(len(reshape) - 1)
+        ]
+
+    return atomic_parameter
 
 
 def write_atomic_model(path, model=gemmi.Model("model")):
--- tests/test_atomic_models.py
+++ tests/test_atomic_models.py
@@ -3,9 +3,15 @@
 import os
 
 import gemmi
+import numpy as np
 import pytest
 
-from ..ioSPI.atomic_models import read_atomic_model, write_atomic_model
+from ..ioSPI.atomic_models import (
+    extract_atomic_parameter,
+    extract_gemmi_atoms,
+    read_atomic_model,
+    write_atomic_model,
+)
 
 DATA = "tests/data"
 OUT = ""
@@ -54,6 +60,33 @@ class TestAtomicModels:
         os.system(f"wget https://files.rcsb.org/download/{cif_filename} -P {DATA}")
         model = read_atomic_model(path)
         assert model.__class__ is gemmi.Model
+
+    def test_extract_gemmi_atoms(self):
+        """Check that extraction retrieves correct no. of chains."""
+        pdb_filename = "2dhb.pdb"
+        path = os.path.join(DATA, pdb_filename)
+        os.system(f"wget https://files.rcsb.org/download/{pdb_filename} -P {DATA}")
+        model = read_atomic_model(path, assemble=False)
+        atoms = extract_gemmi_atoms(model, chains=None, split_chains=True)
+        assert len(atoms) == 2  # expecting two chains
+        atoms = extract_gemmi_atoms(model, chains=["A"], split_chains=True)
+        assert len(atoms) == 1  # expecting one chain
+        atoms = extract_gemmi_atoms(model)
+        assert len(atoms) == 2201  # number of atoms, post-cleaning
+
+    def test_extract_atomic_parameters(self):
+        """Check that form factors are same for same element type."""
+        pdb_filename = "2dhb.pdb"
+        path = os.path.join(DATA, pdb_filename)
+        os.system(f"wget https://files.rcsb.org/download/{pdb_filename} -P {DATA}")
+        model = read_atomic_model(path, assemble=False)
+        atoms = extract_gemmi_atoms(model)
+        indices = [i for i in range(len(atoms)) if atoms[i].element.name == "N"]
+
+        for ptype in ["electron_form_factor_a", "electron_form_factor_b"]:
+            params = extract_atomic_parameter(atoms, ptype)
+            ffs_N = np.array(params)[np.array(indices).astype(int)]
+            assert np.allclose(ffs_N[0], ffs_N)
 
     def test_write_atomic_model_to_pdb(self):
         """Test test_write_gemmi_model_pdb."""
