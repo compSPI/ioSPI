ba8329046af95e3603867a164911b743ed9c27a3
artajmir3 <artajmir3@yahoo.com>
2022-05-06 16:31:47 -0700

    Added cubic option

--- ioSPI/density_maps.py
+++ ioSPI/density_maps.py
@@ -1,15 +1,31 @@
 """Read density maps."""
 
 import mrcfile
+import math
+import numpy as np
 
 
-def read_density_map_from_mrc(path):
+def read_density_map_from_mrc(path, make_cubic=False):
     """Return density map from an input .mrc file.
 
     Parameters
     ----------
     path : str
         File name for .mrc file to convert to density map
+    make_cubic : bool
+        If true, pads the map to reach a cubic array
     """
     map_mrc = mrcfile.open(path)
-    return map_mrc.data
+    data = map_mrc.data
+    if make_cubic:
+        cube_length = max(data.shape)
+        cubic_data = np.pad(data,
+                            ((math.floor((cube_length - data.shape[0])/2.),
+                              math.ceil((cube_length - data.shape[0])/2)),
+                             (math.floor((cube_length - data.shape[1])/2.),
+                              math.ceil((cube_length - data.shape[1])/2)),
+                             (math.floor((cube_length - data.shape[2])/2.),
+                              math.ceil((cube_length - data.shape[2])/2))),
+                            'minimum')
+        data = cubic_data
+    return data
--- tests/test_density_maps.py
+++ tests/test_density_maps.py
@@ -22,3 +22,21 @@ def test_read_density_map_from_mrc():
         assert (out_data == data).all()
     finally:
         os.unlink(tmp_mrc.name)
+
+
+def test_read_density_map_from_mrc_cubic():
+    """Test read_density_map_from_mrc util function make_cubic function."""
+    tmp_mrc = tempfile.NamedTemporaryFile(delete=False, suffix=".mrc")
+    tmp_mrc.close()
+    data = np.random.uniform(0, 1, (4, 5, 6)).astype(dtype='float32')
+
+    try:
+        with mrcfile.new(tmp_mrc.name, overwrite=True) as mrc:
+            mrc.set_data(data)
+        out_data = density_maps.read_density_map_from_mrc(tmp_mrc.name,
+                                                          make_cubic=True)
+        assert (out_data.shape[0] == 6)
+        assert (out_data.shape[1] == 6)
+        assert (out_data.shape[2] == 6)
+    finally:
+        os.unlink(tmp_mrc.name)
