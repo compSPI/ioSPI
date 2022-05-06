d6efbc7447bdcfcbdb3805bb5205284ce4cd497f
artajmir3 <artajmir3@yahoo.com>
2022-05-06 16:37:22 -0700

    Fixed black issues

--- ioSPI/density_maps.py
+++ ioSPI/density_maps.py
@@ -19,13 +19,23 @@ def read_density_map_from_mrc(path, make_cubic=False):
     data = map_mrc.data
     if make_cubic:
         cube_length = max(data.shape)
-        cubic_data = np.pad(data,
-                            ((math.floor((cube_length - data.shape[0])/2.),
-                              math.ceil((cube_length - data.shape[0])/2)),
-                             (math.floor((cube_length - data.shape[1])/2.),
-                              math.ceil((cube_length - data.shape[1])/2)),
-                             (math.floor((cube_length - data.shape[2])/2.),
-                              math.ceil((cube_length - data.shape[2])/2))),
-                            'minimum')
+        cubic_data = np.pad(
+            data,
+            (
+                (
+                    math.floor((cube_length - data.shape[0]) / 2.0),
+                    math.ceil((cube_length - data.shape[0]) / 2),
+                ),
+                (
+                    math.floor((cube_length - data.shape[1]) / 2.0),
+                    math.ceil((cube_length - data.shape[1]) / 2),
+                ),
+                (
+                    math.floor((cube_length - data.shape[2]) / 2.0),
+                    math.ceil((cube_length - data.shape[2]) / 2),
+                ),
+            ),
+            "minimum",
+        )
         data = cubic_data
     return data
--- tests/test_density_maps.py
+++ tests/test_density_maps.py
@@ -13,7 +13,7 @@ def test_read_density_map_from_mrc():
     """Test read_density_map_from_mrc util function with a random mrc file."""
     tmp_mrc = tempfile.NamedTemporaryFile(delete=False, suffix=".mrc")
     tmp_mrc.close()
-    data = np.random.uniform(0, 1, (5, 5, 5)).astype(dtype='float32')
+    data = np.random.uniform(0, 1, (5, 5, 5)).astype(dtype="float32")
 
     try:
         with mrcfile.new(tmp_mrc.name, overwrite=True) as mrc:
@@ -28,15 +28,14 @@ def test_read_density_map_from_mrc_cubic():
     """Test read_density_map_from_mrc util function make_cubic function."""
     tmp_mrc = tempfile.NamedTemporaryFile(delete=False, suffix=".mrc")
     tmp_mrc.close()
-    data = np.random.uniform(0, 1, (4, 5, 6)).astype(dtype='float32')
+    data = np.random.uniform(0, 1, (4, 5, 6)).astype(dtype="float32")
 
     try:
         with mrcfile.new(tmp_mrc.name, overwrite=True) as mrc:
             mrc.set_data(data)
-        out_data = density_maps.read_density_map_from_mrc(tmp_mrc.name,
-                                                          make_cubic=True)
-        assert (out_data.shape[0] == 6)
-        assert (out_data.shape[1] == 6)
-        assert (out_data.shape[2] == 6)
+        out_data = density_maps.read_density_map_from_mrc(tmp_mrc.name, make_cubic=True)
+        assert out_data.shape[0] == 6
+        assert out_data.shape[1] == 6
+        assert out_data.shape[2] == 6
     finally:
         os.unlink(tmp_mrc.name)
