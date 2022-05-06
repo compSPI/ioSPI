ce2db91b1f9770134bc4c5958918dd1a24e5cfa6
artajmir3 <artajmir3@yahoo.com>
2022-05-06 14:03:28 -0700

    Added initial simple code for density maps

---
+++ ioSPI/density_maps.py
@@ -0,0 +1,19 @@
+"""Read density maps."""
+
+import os
+
+import mrcfile
+import numpy as np
+
+
+def read_density_map_from_mrc(path):
+    """Return density map from an input .mrc file.
+
+    Parameters
+    ----------
+    path : str
+        File name for .mrc file to convert to density map
+    """
+    map_mrc = mrcfile.open(path)
+    return map_mrc.data
+    
---
+++ tests/test_density_maps.py
@@ -0,0 +1,24 @@
+"""Unit tests for density maps helper functions."""
+
+import os
+import tempfile
+
+import mrcfile
+import numpy as np
+
+from ..ioSPI import density_maps
+
+
+def test_read_density_map_from_mrc():
+    """Test read_density_map_from_mrc util function with a random small mrc file."""
+    tmp_mrc = tempfile.NamedTemporaryFile(delete=False, suffix=".mrc")
+    tmp_mrc.close()
+    data = np.random.uniform(0, 1, (5, 5, 5)).astype(dtype='float32')
+
+    try:
+        with mrcfile.new(tmp_mrc.name, overwrite=True) as mrc:
+            mrc.set_data(data)
+        out_data = density_maps.read_density_map_from_mrc(tmp_mrc.name)
+        assert (out_data == data).all()
+    finally:
+        os.unlink(tmp_mrc.name)
