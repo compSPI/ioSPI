39344f77752b244cc85f3e5f97fc2530e8d2ec26
Bongjin Koo <b.misc@icloud.com>
2022-05-12 17:38:17 -0700

    Issue #74: Add a delete file function in datasets.py. Add method args error checks in datasets.py. Update test_datasets.py.

--- ioSPI/datasets.py
+++ ioSPI/datasets.py
@@ -4,7 +4,7 @@ import os
 
 
 class OSFProject:
-    """Class to list, download and upload data from an OSF project using osfclient library.
+    """Class to list, download and upload data in an OSF project using osfclient library.
 
     Parameters
     ----------
@@ -14,9 +14,11 @@ class OSFProject:
     token : str
         Personal token from osf.io.
         See: https://osf.io/settings/tokens
-    project_id : str, default = "7g42j"
+    project_id : str, default = "xbr2m"
         Identifier of the project, found on the OSF project page.
-        E.g. 7g42j for project at https://osf.io/7g42j/
+        E.g. xbr2m for project at https://osf.io/xbr2m/
+    storage : str, default = "osfstorage"
+        Storage provider of the project.
 
     See Also
     --------
@@ -24,17 +26,31 @@ class OSFProject:
     OSF API documentation : https://developer.osf.io/
     """
 
-    def __init__(self, username: str, token: str, project_id: str = "7g42j") -> None:
+    def __init__(
+        self,
+        username: str,
+        token: str,
+        project_id: str = "xbr2m",
+        storage: str = "osfstorage",
+    ) -> None:
+        if username is None:
+            raise ValueError("username must be provided.")
         self.username = username
+
+        if token is None:
+            raise ValueError("token must be provided.")
         self.token = token
+
         self.project_id = project_id
+        self.storage = storage
 
         config_path = os.path.join(".osfcli.config")
         with open(config_path, "w") as out_file:
             out_file.write("[osf]\n")
             out_file.write(f"username = {username}\n")
-            out_file.write(f"project = {project_id}\n")
             out_file.write(f"token = {token}\n")
+            out_file.write(f"project = {project_id}\n")
+            out_file.write(f"storage = {storage}\n")
         print("OSF config written to .osfcli.config!")
 
     def ls(self):
@@ -42,14 +58,14 @@ class OSFProject:
         print(f"Listing files from OSF project: {self.project_id}...")
         os.system("osf ls")
 
-    @staticmethod
-    def download(remote_path, local_path):
-        """Download file from osf and save it locally.
+    def download(self, remote_path: str, local_path: str):
+        """Download a file from an OSF project and save it locally.
 
         Parameters
         ----------
         remote_path : str
-            Remote path of the file on OSF.
+            Remote path of the file in an OSF project,
+            which will be appended to the project storage name (by default, osfstorage).
             E.g. osfstorage/
             randomrot1D_nodisorder/
             4v6x_randomrot_copy6_defocus3.0_yes_noise.txt
@@ -57,13 +73,18 @@ class OSFProject:
             Local path where the file will be saved.
             E.g. 4v6x_randomrot_copy6_defocus3.0_yes_noise.txt
         """
-        print(f"Downloading {remote_path} to {local_path}...")
-        os.system(f"osf fetch {remote_path} {local_path}")
+        if remote_path is None:
+            raise ValueError("remote_path must be provided.")
+        if local_path is None:
+            raise ValueError("local_path must be provided.")
+
+        full_remote_path = self.storage + "/" + remote_path
+        print(f"Downloading {full_remote_path} to {local_path}...")
+        os.system(f"osf fetch {full_remote_path} {local_path}")
         print("Done!")
 
-    @staticmethod
-    def upload(remote_path, local_path):
-        """Upload file to osf.
+    def upload(self, local_path: str, remote_path: str):
+        """Upload a file to an OSF project.
 
         Notes
         -----
@@ -71,15 +92,42 @@ class OSFProject:
 
         Parameters
         ----------
-        remote_path : str
-            Remote path of the file on OSF.
-            E.g. osfstorage/
-            randomrot1D_nodisorder/
-            4v6x_randomrot_copy6_defocus3.0_yes_noise.txt
         local_path : str
             Local path where the file will be saved.
             E.g. 4v6x_randomrot_copy6_defocus3.0_yes_noise.txt
+        remote_path : str
+            Remote path of the file in an OSF project,
+            which will be appended to the project storage name (by default, osfstorage).
+            E.g. osfstorage/
+            randomrot1D_nodisorder/
+            4v6x_randomrot_copy6_defocus3.0_yes_noise.txt
         """
-        print(f"Uploading {local_path} to {remote_path}...")
-        os.system(f"osf upload {local_path} {remote_path}")
+        if local_path is None:
+            raise ValueError("local_path must be provided.")
+        if remote_path is None:
+            raise ValueError("remote_path must be provided.")
+
+        full_remote_path = self.storage + "/" + remote_path
+        print(f"Uploading {local_path} to {full_remote_path}...")
+        os.system(f"osf upload {local_path} {full_remote_path}")
+        print("Done!")
+
+    def remove(self, remote_path: str):
+        """Remove a file in an OSF project.
+
+        Parameters
+        ----------
+        remote_path : str
+            Remote path of the file to remove in an OSF project,
+            which will be appended to the project storage name (by default, osfstorage).
+            E.g. osfstorage/
+            randomrot1D_nodisorder/
+            4v6x_randomrot_copy6_defocus3.0_yes_noise.txt
+        """
+        if remote_path is None:
+            raise ValueError("remote_path must be provided.")
+
+        full_remote_path = self.storage + "/" + remote_path
+        print(f"Removing {full_remote_path} in the project...")
+        os.system(f"osf remove {full_remote_path}")
         print("Done!")
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,168 +1,108 @@
-"""Tests for osf_upload."""
+"""Unit tests for listing/uploading/downloading datasets on an OSF project."""
+
 import os
-import random
-import string
-from pathlib import Path
 
 import pytest
-import requests
 
 from ioSPI import datasets
 
 
 @pytest.fixture(autouse=True, scope="session")
-def setup_teardown():
+def setup():
     """Test node creation and clean-up for tests."""
-    token = os.environ["TEST_TOKEN"]
-    request_headers = {"Authorization": f"Bearer {token}"}
-    base_api_url = "https://api.osf.io/v2/nodes/"
-
-    test_node_label = "test_" + "".join(
-        random.choice(string.ascii_letters) for i in range(5)
+    print(f"Creating a test project for dataset")
+    project = datasets.OSFProject(
+        username="ninamio78@gmail.com",
+        token="HBGGBOJcLYQfadEKIOyXJiLTum3ydXK4nGP3KmbkYUeBuYkZma9LPBSYennQn92gjP2NHn",
+        project_id="xbr2m",
     )
 
-    print(f"Creating test node Dataset -> internal -> {test_node_label} ")
-
-    internal_node_guid = "9jwpu"
-    request_url = f"{base_api_url}{internal_node_guid}/children/"
-
-    request_body = {
-        "type": "nodes",
-        "attributes": {"title": test_node_label, "category": "data"},
-    }
-
-    response = requests.post(
-        request_url, headers=request_headers, json={"data": request_body}
-    )
-    response.raise_for_status()
-
-    pytest.auth_token = token
-    pytest.test_node_guid = response.json()["data"]["id"]
-    pytest.test_node_label = test_node_label
-    pytest.request_headers = request_headers
-    pytest.base_api_url = base_api_url
-
-    yield
-
-    print(
-        f"\nDeleting test node Dataset -> internal -> "
-        f"{test_node_label} and its sub-components."
-    )
-    cleanup(pytest.test_node_guid, test_node_label)
-
-
-def cleanup(node_guid, test_node_label):
-    """Recursively delete nodes and subcomponents."""
-    base_node_url = f"{pytest.base_api_url}{node_guid}/"
-
-    response = requests.get(f"{base_node_url}children/", headers=pytest.request_headers)
-    response.raise_for_status()
-
-    for node_child in response.json()["data"]:
-        cleanup(node_child["id"], node_child["attributes"]["title"])
-
-    response = requests.delete(base_node_url, headers=pytest.request_headers)
-
-    if not response.ok:
-        print(
-            f"Failure: {test_node_label} could not be"
-            f" deleted due to error code {response.status_code}."
-        )
-        print(response.json()["errors"][0]["detail"])
-    else:
-        print(f"Success: {test_node_label} deleted")
+    yield project
 
 
 @pytest.fixture
-def mock_osf_upload():
-    """Return OSFUpload for testing."""
-    return datasets.OSFUpload(pytest.auth_token, pytest.test_node_guid)
-
-
-@pytest.fixture
-def create_upload_file(tmp_path):
-    """Create temporary text file for upload."""
-    file_path = Path(tmp_path, "test_upload_1.txt")
+def create_upload_file():
+    """Create a temporary text file for upload."""
+    file_path = "test_upload.txt"
     with open(file_path, "w") as f:
         f.write("Hello World")
     return file_path
 
 
-def test_constructor():
-    """Test constructor populates class attributes."""
-    test_class = datasets.OSFUpload(pytest.auth_token)
-
-    assert test_class.headers is not None
-    assert test_class.base_url is not None
-    assert test_class.data_node_guid is not None
-
-
-def test_write_child_node(mock_osf_upload):
-    """Test whether nodes are created."""
-    test_node_label = "test_write_child_node" + "".join(
-        random.choice(string.ascii_letters) for i in range(5)
-    )
-    returned_guid = mock_osf_upload.write_child_node(
-        pytest.test_node_guid, test_node_label
-    )
-    response = requests.get(
-        f"{pytest.base_api_url}{returned_guid}", headers=pytest.request_headers
-    )
-    assert test_node_label == response.json()["data"]["attributes"]["title"]
+def test_constructor_valid():
+    """Test the constructor."""
+    project = datasets.OSFProject(username="username", token="token")
+    assert project.username is not None
+    assert project.token is not None
+    assert project.project_id is not None
+    assert project.storage is not None
 
 
-def test_read_existing_structure_labels(mock_osf_upload):
-    """Test if uploaded test node is retrieved."""
-    request_url = f"{pytest.base_api_url}{pytest.test_node_guid}/children/"
-    test_node_label = "test_read_existing_structure_labels_" + "".join(
-        random.choice(string.ascii_letters) for i in range(5)
-    )
-    request_body = {
-        "type": "nodes",
-        "attributes": {"title": test_node_label, "category": "data"},
-    }
-
-    requests.post(
-        request_url, headers=pytest.request_headers, json={"data": request_body}
-    ).raise_for_status()
-
-    assert test_node_label in mock_osf_upload.read_existing_structure_labels()
+def test_constructor_invalid_because_no_username():
+    """Test if an error is raised when no username is provided to the constructor."""
+    with pytest.raises(ValueError):
+        _ = datasets.OSFProject(token="token")
 
 
-def test_read_structure_guid(mock_osf_upload):
-    """Test if valid guid is returned for a structure's label."""
-    test_node_label = "test_read_structure_guid" + "".join(
-        random.choice(string.ascii_letters) for i in range(5)
-    )
-
-    returned_guid = mock_osf_upload.read_structure_guid(test_node_label)
-    assert returned_guid is None
-
-    request_url = f"{pytest.base_api_url}{pytest.test_node_guid}/children/"
-    request_body = {
-        "type": "nodes",
-        "attributes": {"title": test_node_label, "category": "data", "public": True},
-    }
-    response = requests.post(
-        request_url, headers=pytest.request_headers, json={"data": request_body}
-    )
-    response.raise_for_status()
-
-    assert response.json()["data"]["id"] == mock_osf_upload.read_structure_guid(
-        test_node_label
-    )
+def test_constructor_invalid_because_no_token():
+    """Test if an error is raised when no token is provided to the constructor."""
+    with pytest.raises(ValueError):
+        datasets.OSFProject(username="username")
 
 
-def test_write_files(mock_osf_upload, create_upload_file):
-    """Test whether files are uploaded to OSF."""
-    assert mock_osf_upload.write_files(pytest.test_node_guid, [str(create_upload_file)])
+def test_upload_valid(setup, create_upload_file):
+    """Test the upload method."""
+    setup.upload(create_upload_file, create_upload_file)
+    out = os.popen("osf ls").read()
+    file_exists = False
+    for file in out:
+        file_exists = create_upload_file == file.split("/")[1]
 
-    response = requests.get(
-        f"{pytest.base_api_url}{pytest.test_node_guid}/files/osfstorage/",
-        headers=pytest.request_headers,
-    )
+    assert file_exists
 
-    response.raise_for_status()
-    parsed_response = response.json()["data"][0]
 
-    assert Path(create_upload_file).name == parsed_response["attributes"]["name"]
+def test_upload_invalid_because_no_local_path(setup):
+    """Test if an error is raised when no local_path is provided."""
+    with pytest.raises(ValueError):
+        setup.upload(remote_path="remote_path")
+
+
+def test_upload_invalid_because_no_remote_path(setup):
+    """Test if an error is raised when no remote_path is provided."""
+    with pytest.raises(ValueError):
+        setup.upload(local_path="local_path")
+
+
+def test_download_valid(setup, create_upload_file):
+    """Test the download method."""
+    setup.download(create_upload_file, create_upload_file)
+    assert os.path.exists(create_upload_file)
+
+
+def test_download_invalid_because_no_remote_path(setup):
+    """Test if an error is raised when no remote_path is provided."""
+    with pytest.raises(ValueError):
+        setup.download(local_path="local_path")
+
+
+def test_download_invalid_because_no_local_path(setup):
+    """Test if an error is raised when no local_path is provided."""
+    with pytest.raises(ValueError):
+        setup.download(remote_path="remote_path")
+
+
+def test_remove_valid(setup, create_upload_file):
+    """Test the remove method."""
+    setup.remove(create_upload_file)
+    out = os.popen("osf ls").read()
+    file_exists = False
+    for file in out:
+        file_exists = create_upload_file == file.split("/")[1]
+
+    assert not file_exists
+
+
+def test_remove_invalid_because_no_remote_path(setup):
+    """Test if an error is raised when no remote_path is provided."""
+    with pytest.raises(ValueError):
+        setup.remove()
